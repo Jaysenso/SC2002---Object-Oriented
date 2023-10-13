e7929a254f1b9c436403e4ca0f29997f621d8409
Jaysenso <jsenongxuan@gmail.com>
2023-10-14 02:32:49 +0800

    Update ClassX.java

--- PYPs/2020-2021 SEM2/UpcastingQ2b/ClassX.java
+++ PYPs/2020-2021 SEM2/UpcastingQ2b/ClassX.java
@@ -1,6 +1,6 @@
 
 public class ClassX {
-    void compare(int a, int b){
+    void compare(int a, double b){
         if(a == b) System.out.println("Equal in X");
         else System.out.println("Different in X");
     }
