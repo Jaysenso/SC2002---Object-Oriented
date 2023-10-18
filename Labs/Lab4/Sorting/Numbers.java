d2a8d55b17563905e9c3a9731626f350f1fdba20
Jaysenso <jsenongxuan@gmail.com>
2023-10-18 12:50:50 +0800

    Update Numbers.java

--- Labs/Lab4/Sorting/Numbers.java
+++ Labs/Lab4/Sorting/Numbers.java
@@ -17,8 +17,9 @@ public static void main (String[] args)
 
 	for (int i = 0; i < size; i++)
 		intList[i] = scan.nextInt();
-		//Sorting.selectionSort(intList);
-		Sorting.insertionSort(intList);
+
+	//Sorting.selectionSort(intList);
+	Sorting.insertionSort(intList);
 
 		System.out.println ("\nYour numbers in sorted order...");
 	for (int i = 0; i < size; i++){
